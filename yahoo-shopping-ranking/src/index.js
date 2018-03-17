e2abc32c58d975c4023fd22340ea70e83f8b3cf1
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-03-17 23:00:28 +0900

    App bootstrap

--- yahoo-shopping-ranking/src/index.js
+++ yahoo-shopping-ranking/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+  combineReducers(reducers),
+  applyMiddleware(logger),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
