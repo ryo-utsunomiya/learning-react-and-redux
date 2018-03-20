62916e2aeeed6b012e8d22910e090717de7ae0e3
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-03-21 00:31:17 +0900

    Navigation by redux store

--- yahoo-shopping-ranking/src/App.js
+++ yahoo-shopping-ranking/src/App.js
@@ -1,31 +1,23 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Ranking from './containers/Ranking';
+import Nav from './containers/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* Hard-code category id */}
-        <ul>
-          <li>
-            <Link to="/all">All categories</Link>
-          </li>
-          <li>
-            <Link to="/category/2502">PC</Link>
-          </li>
-          <li>
-            <Link to="/category/10002">Books</Link>
-          </li>
-        </ul>
+        <Nav />
 
-        {/* General ranking routing */}
-        <Route path="/all" component={Ranking} />
-        <Route
-          path="/category/:id"
-          render={({ match }) => <Ranking categoryId={match.params.id} />}
-        />
+        <Switch>
+          <Route path="/all" component={Ranking} />
+          <Route path="/category/1" render={() => <Redirect to="/all" />} />
+          <Route
+            path="/category/:id"
+            render={({ match }) => <Ranking categoryId={match.params.id} />}
+          />
+        </Switch>
       </div>
     );
   }
---
+++ yahoo-shopping-ranking/src/reducers/Ranking.js
@@ -0,0 +1,42 @@
+const getRanking = response => {
+  const ranking = [];
+  const itemLength = response.ResultSet.totalResultsReturned;
+  for (let i = 0; i < itemLength; i++) {
+    const item = response.ResultSet['0'].Result[i.toString()];
+    ranking.push({
+      code: item.Code,
+      name: item.Name,
+      url: item.Url,
+      imageUrl: item.Image.Medium,
+    });
+  }
+  return ranking;
+};
+
+const initialState = {
+  categoryId: undefined,
+  ranking: undefined,
+  error: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'START_REQUEST':
+      return {
+        categoryId: action.payload.categoryId,
+        ranking: undefined,
+        error: false,
+      };
+
+    case 'RECEIVE_DATA':
+      return action.payload.error
+        ? { ...state, error: true }
+        : {
+          ...state,
+          ranking: getRanking(action.payload.response),
+        };
+
+    default:
+      return state;
+  }
+};
