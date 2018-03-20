5f2324a9f2733fd39c37a918b90dd25523abcb7f
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-03-20 23:04:44 +0900

    Add thunk middleware

--- yahoo-shopping-ranking/src/createStore.js
+++ yahoo-shopping-ranking/src/createStore.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
+
 import * as reducers from './reducers';
 
 // history is passed by src/index.js.
@@ -8,12 +10,7 @@ export default history =>
   createStore(
     combineReducers({
       ...reducers,
-      // reducer of react-router-redux
       router: routerReducer,
     }),
-    applyMiddleware(
-      logger,
-      // redux middleware of react-router-redux
-      routerMiddleware(history),
-    ),
+    applyMiddleware(logger, thunk, routerMiddleware(history)),
   );
