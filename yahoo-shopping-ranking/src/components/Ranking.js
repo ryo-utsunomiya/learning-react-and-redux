0d697b2c3b1b95d1963bf596967cb84ec3d041c6
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-03-20 23:16:21 +0900

    Add Ranking lifecycle method

--- yahoo-shopping-ranking/src/components/Ranking.js
+++ yahoo-shopping-ranking/src/components/Ranking.js
@@ -5,16 +5,30 @@ const CATEGORY_ID = {
   GENERAL: '1',
 };
 
-export default function Ranking({ categoryId }) {
-  return (
-    <div>
-      <h2>Ranking Component</h2>
-      <p>Category ID: {categoryId}</p>
-    </div>
-  );
+export default class Ranking extends React.Component {
+  componentWillMount() {
+    this.props.onMount(this.props.categoryId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.categoryId !== nextProps.categoryId) {
+      this.props.onUpdate(nextProps.categoryId);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Ranking Component</h2>
+        <p>Category ID: {this.props.categoryId}</p>
+      </div>
+    );
+  }
 }
 Ranking.propTypes = {
   categoryId: PropTypes.string,
+  onMount: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 Ranking.defaultProps = {
   categoryId: CATEGORY_ID.GENERAL,
